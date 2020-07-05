551bf17aee887ff125722edeeed49c42ecff4538
Takashi Kokubun <takashikkbn@gmail.com>
2020-07-05 15:28:42 -0700

    Fix the version trick

--- mysqldef.rb
+++ mysqldef.rb
@@ -1,12 +1,11 @@
 class Mysqldef < Formula
-  version = 'v0.5.20'
   desc 'The easiest idempotent schema management by SQL.'
   homepage 'https://github.com/k0kubun/sqldef'
-  url 'https://github.com/k0kubun/sqldef.git', tag: version
+  url 'https://github.com/k0kubun/sqldef.git', tag: 'v0.5.20'
   head 'https://github.com/k0kubun/sqldef.git'
 
   def install
-    system 'curl', '-o', 'mysqldef.zip', '-sL', "https://github.com/k0kubun/sqldef/releases/download/#{version}/mysqldef_darwin_amd64.zip"
+    system 'curl', '-o', 'mysqldef.zip', '-sL', "https://github.com/k0kubun/sqldef/releases/download/v#{version}/mysqldef_darwin_amd64.zip"
     system 'unzip', 'mysqldef.zip'
     bin.install 'mysqldef'
   end
