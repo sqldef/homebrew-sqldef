7f9516f3a1970328d109a6604353b6c0d7e77e91
sqldef <k0kubun@github.com>
2022-02-17 08:51:38 +0000

    Version v0.11.38

--- psqldef.rb
+++ psqldef.rb
@@ -1,7 +1,7 @@
 class Psqldef < Formula
   desc 'The easiest idempotent schema management by SQL.'
   homepage 'https://github.com/k0kubun/sqldef'
-  url 'https://github.com/k0kubun/sqldef.git', tag: 'v0.11.37'
+  url 'https://github.com/k0kubun/sqldef.git', tag: 'v0.11.38'
   head 'https://github.com/k0kubun/sqldef.git'
 
   def install
