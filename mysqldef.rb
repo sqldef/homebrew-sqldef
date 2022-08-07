0ecbb7f876cc6f768dd7612ed795c07fceeadb74
sqldef <k0kubun@github.com>
2022-08-07 16:00:27 +0000

    Version v0.13.0

--- mysqldef.rb
+++ mysqldef.rb
@@ -1,7 +1,7 @@
 class Mysqldef < Formula
   desc 'The easiest idempotent schema management by SQL.'
   homepage 'https://github.com/k0kubun/sqldef'
-  url 'https://github.com/k0kubun/sqldef.git', tag: 'v0.12.8'
+  url 'https://github.com/k0kubun/sqldef.git', tag: 'v0.13.0'
   head 'https://github.com/k0kubun/sqldef.git'
 
   def install
