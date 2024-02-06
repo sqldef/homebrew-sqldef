94f7e31460264a7a865c29deea1698238f737f3a
Lars Corneliussen <lc@talendos.com>
2024-02-06 10:42:18 +0100

    Add formula for mssqldef

---
+++ mssqldef.rb
@@ -0,0 +1,19 @@
+class Mssqldef < Formula
+  desc 'The easiest idempotent schema management by SQL.'
+  homepage 'https://github.com/k0kubun/sqldef'
+  url 'https://github.com/k0kubun/sqldef.git', tag: 'v0.16.15'
+  head 'https://github.com/k0kubun/sqldef.git'
+
+  def install
+    os = `uname -s`.strip.downcase
+    arch = {
+      'x86_64'  => 'amd64',
+      'aarch64' => 'arm64',
+      'arm64'   => 'arm64',
+    }.fetch(`uname -m`.strip)
+
+    system 'curl', '-o', 'mssqldef.zip', '-sL', "https://github.com/k0kubun/sqldef/releases/download/v#{version}/mssqldef_#{os}_#{arch}.zip"
+    system 'unzip', 'mssqldef.zip'
+    bin.install 'mssqldef'
+  end
+end
