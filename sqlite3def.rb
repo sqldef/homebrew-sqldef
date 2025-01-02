41aa04fac4f2ca7971d564ffdc0e938291194257
sqldef <k0kubun@github.com>
2025-01-02 20:40:14 +0000

    Version v0.17.25

--- sqlite3def.rb
+++ sqlite3def.rb
@@ -1,7 +1,7 @@
 class Sqlite3def < Formula
   desc 'The easiest idempotent schema management by SQL.'
   homepage 'https://github.com/sqldef/sqldef'
-  url 'https://github.com/sqldef/sqldef.git', tag: 'v0.17.24'
+  url 'https://github.com/sqldef/sqldef.git', tag: 'v0.17.25'
   head 'https://github.com/sqldef/sqldef.git'
 
   def install
