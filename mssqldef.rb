fd85ba1d10e4f68a9bf9a183ac28ac8bf30579f4
sqldef <k0kubun@github.com>
2026-02-05 21:19:57 +0000

    Version v3.9.7

--- mssqldef.rb
+++ mssqldef.rb
@@ -1,7 +1,7 @@
 class Mssqldef < Formula
   desc 'The easiest idempotent schema management by SQL.'
   homepage 'https://github.com/sqldef/sqldef'
-  url 'https://github.com/sqldef/sqldef.git', tag: 'v3.9.6'
+  url 'https://github.com/sqldef/sqldef.git', tag: 'v3.9.7'
   head 'https://github.com/sqldef/sqldef.git'
 
   def install
